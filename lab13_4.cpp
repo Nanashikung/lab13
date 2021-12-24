d05675d03972c2448235d2cdb07f23beb61a8d76
Nanashikung <nata.station@hotmail.com>
2021-12-24 15:14:40 +0700

    lab13_4 complete

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename mS>
+void mySwap(mS&,mS&);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,10 @@ int main(){
 	return 0;
 }
 
+template <typename mS>
+void mySwap(mS &x,mS &y){
+	mS a;
+	a = x;
+	x = y;
+	y = a;
+}
